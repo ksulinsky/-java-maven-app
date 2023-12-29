cdfc1761a113adfc020b0c8b39ff306f07217612
ksulinsky <62070703+ksulinsky@users.noreply.github.com>
2023-12-29 15:22:20 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,17 +1,16 @@
 def build_app() {
-             echo 'Building...'
-             sh "mvn package"
+    echo 'Building...'
+    sh "mvn package"
 }
 
 def buildDockerImage() {
-         // Bind the Docker credentials
-            withCredentials([usernamePassword(credentialsId: 'docker-credentials', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
-            echo 'Building docker image and pushing it...'
-            sh "docker build -t maven-java-app:1.0 ."
-            sh "docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD"
-            sh 'docker push ksulinsky/repository:maven-java-app'
-                    }
-
+    // Bind the Docker credentials
+    withCredentials([usernamePassword(credentialsId: 'docker-credentials', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
+        echo 'Building docker image and pushing it...'
+        sh "docker build -t maven-java-app:1.0 ."
+        sh "docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD"
+        sh 'docker push ksulinsky/repository:maven-java-app'
+    }
 }
 
 return this
