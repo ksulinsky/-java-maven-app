44411a85f95c5474d50017000c950b0b0edd933f
ksulinsky <62070703+ksulinsky@users.noreply.github.com>
2024-01-01 22:12:34 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -16,8 +16,11 @@ def buildDockerImage() {
 def incrementVersion() {
     script {
         // Increment the major version using the versions-maven-plugin and build-helper-maven-plugin
-sh "mvn build-helper:parse-version versions:set -DnewVersion=${parsedVersion.majorVersion + 1}.${parsedVersion.minorVersion}.${parsedVersion.incrementalVersion} versions:commit"
-
+        sh '''
+            mvn build-helper:parse-version versions:set \
+                -DnewVersion=${parsedVersion.majorVersion + 1}.${parsedVersion.minorVersion}.${parsedVersion.incrementalVersion} \
+                versions:commit
+        '''
     }
 }
 
