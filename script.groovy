bc9b9ce1cdd46bf9fd1b626a3c3b3077197bf2f7
ksulinsky <62070703+ksulinsky@users.noreply.github.com>
2024-01-02 11:06:17 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,22 +5,18 @@ def build_app() {
 
 def buildDockerImage() {
     // Bind the Docker credentials
+    def mavenVersion = sh(script: 'mvn help:evaluate -Dexpression=project.version -q -DforceStdout', returnStdout: true).trim()
+    def dockerImageTag = "ksulinsky/repository:jma-${mavenVersion}"
+    echo "Building docker image with tag: ${dockerImageTag}"
+
     withCredentials([usernamePassword(credentialsId: 'docker-credentials', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
         echo 'Building docker image and pushing it...'
-        sh "docker build -t maven-java-app:1.0 ."
+        sh "docker build -t maven-java-app:${dockerImageTag} ."
         sh "docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD"
-        sh 'docker push ksulinsky/repository:jma-1.0'
+        sh "docker push ${dockerImageTag} "
     }
 }
 
-def incrementVersion() {
 
-        // Increment the major version using the versions-maven-plugin and build-helper-maven-plugin
-        sh '''
-            mvn build-helper:parse-version versions:set \
-                -DnewVersion=${parsedVersion.majorVersion + 1}.${parsedVersion.minorVersion}.${parsedVersion.incrementalVersion} \
-                versions:commit
-        '''
-}
 
 return this
