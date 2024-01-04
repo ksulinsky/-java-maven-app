436e363c972aac4b1684ce6370cab7d891472e05
Krzysztof <kristof@gmail.com>
2024-01-04 13:30:45 +0100

    akakakaka

--- script.groovy
+++ script.groovy
@@ -67,8 +67,8 @@ def deployApplication() {
         // ... other commands ...
         // Convert env.dockerImageTag to lowercase
         //def lowercaseImageTag = env.dockerImageTag.toLowerCase()
-        //ssh user: 'ec2-user', host: '3.70.229.201', command: 'echo "Executing ls -la"'
-        //ssh user: 'ec2-user', host: '3.70.229.201', command: 'ls -la'
+        ssh user: 'ec2-user', host: '3.70.229.201', command: 'echo "Executing ls -la"'
+        ssh user: 'ec2-user', host: '3.70.229.201', command: 'ls -la'
 
         // Debug statement for the Docker command
         //echo "Running Docker command: docker run -p 8080:8080 -d ${lowercaseImageTag}"
